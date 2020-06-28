544c26b1bee88df57f01b8359997ef9db084f08b
Vuong Cuong <vuongcuong.phoenix@gmail.com>
2020-06-28 10:42:31 +0700

    test(composition): tested '@vue/composition-api'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png" />
+        <HelloWorld msg="Welcome to Your Vue.js App" />
+    </div>
 </template>
 
-<script>
+<script lang="ts">
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import { defineComponent } from "@vue/composition-api";
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld
-  }
-};
+export default defineComponent({
+    name: "Home",
+    components: {
+        HelloWorld,
+    },
+});
 </script>
