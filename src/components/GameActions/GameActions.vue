dad46f955139877748c86152748d22b8009d029f
Vuong Cuong <vuongcuong.phoenix@gmail.com>
2020-06-30 11:35:54 +0700

    feat(component): add action buttons

---
+++ src/components/GameActions/GameActions.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="text-center space-x-2 space-y-2">
+        <button
+            class="shadow-md btn btn-primary"
+            @click="random"
+        >Randomize</button>
+        <button
+            class="shadow-md btn btn-primary"
+            @click="start"
+        >Start</button>
+        <!-- <button class="shadow-md btn btn-primary">Pause</button> -->
+        <!-- <button class="shadow-md btn btn-primary">Resume</button> -->
+    </div>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from "@vue/composition-api";
+export default defineComponent({
+    name: "GameActions",
+
+    setup(props, context) {
+        function random() {
+            context.emit("random");
+        }
+
+        function start() {
+            context.emit("start");
+        }
+
+        return {
+            random,
+            start,
+        };
+    },
+});
+</script>
