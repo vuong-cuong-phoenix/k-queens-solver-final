6ef7636013cb29b1370adfce409b68f4f80d4103
Vuong Cuong <vuongcuong.phoenix@gmail.com>
2020-07-03 00:33:28 +0700

    feat(package): setting axios globally

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ import {
 import {} from "@fortawesome/free-regular-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import axios from "axios";
 
 import App from "./App.vue";
 import "./registerServiceWorker";
@@ -24,6 +25,8 @@ Vue.use(VueCompositionAPI);
 
 library.add(faBars, faCrown, faGithub, faHome, faInfoCircle, faChevronDown, faChevronUp, faRunning);
 Vue.component("font-awesome-icon", FontAwesomeIcon);
+
+axios.defaults.baseURL = "http://localhost:5000";
 
 Vue.config.productionTip = false;
 
